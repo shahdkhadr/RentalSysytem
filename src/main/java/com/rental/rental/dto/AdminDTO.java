80df716780779153bb40ac7ef947b5b927abf83e
AbdullahGSholi <asholi@asaltech.com>
2024-10-18 23:21:48 +0300

    feat: 1) CRUD Operations for Admin, Department and Employee Entities. 2) API Documentation for it's API with Swagger.

--- src/main/java/com/rental/rental/dto/AdminDTO.java
+++ src/main/java/com/rental/rental/dto/AdminDTO.java
@@ -1,5 +1,6 @@
 package com.rental.rental.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -10,14 +11,18 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Schema(description = "Data Transfer Object for admin information")
 public class AdminDTO {
 
     @NotEmpty(message = "The admin id is required")
+    @Schema(description = "Unique identifier of the admin", example = "1")
     private int adminId;
 
     @NotEmpty(message = "The name is required")
+    @Schema(description = "Name of the admin", example = "Ali Hamadneh")
     private String adminName;
 
     @NotEmpty(message = "The phone number is required")
+    @Schema(description = "Phone number of the admin", example = "0568347481")
     private String phoneNumber;
 }
--- src/main/java/com/rental/rental/dto/EmployeeDTO.java
+++ src/main/java/com/rental/rental/dto/EmployeeDTO.java
@@ -1,5 +1,6 @@
 package com.rental.rental.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -13,23 +14,26 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Schema(description = "Data Transfer Object for employee information")
 public class EmployeeDTO {
 
     @NotNull(message = "The employee ID is required")
+    @Schema(description = "Unique identifier of the employee", example = "1")
     private int employeeId;
 
     @NotEmpty(message = "The employee name is required")
+    @Schema(description = "Name of the employee", example = "Abdullah Sholi")
     private String employeeName;
 
     @NotEmpty(message = "The employee phone number is required")
+    @Schema(description = "Phone number of the employee", example = "0592659066")
     private String phoneNumber;
 
     @NotEmpty(message = "The employment type is required")
+    @Schema(description = "Type of the employee", example = "Accounting")
     private String employmentType;
 
     @NotNull(message = "Department ID is required")
     private int departmentId;
 
-    @NotEmpty(message = "The employee must have at least one invoice")
-    private List<Integer> invoiceIds;
 }
