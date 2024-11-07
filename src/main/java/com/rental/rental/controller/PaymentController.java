f6f563c8714062a6793b1f647f2647139fc5c65c
shahdkhadr <skhader@asaltech.com>
2024-11-07 10:40:08 +0200

    add role for payment process

--- src/main/java/com/rental/rental/controller/PaymentController.java
+++ src/main/java/com/rental/rental/controller/PaymentController.java
@@ -9,6 +9,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -37,6 +38,7 @@ public class PaymentController {
 
 
     @PostMapping("/process")
+    @PreAuthorize("hasAnyRole('ADMIN', 'CUSTOMER')")
     @Operation(summary = "Process a payment", description = "Process a payment using on Arrival or card with Stripe or PayPal based on the card type")
     public ResponseEntity<Map<String, String>> processPayment(@RequestBody PaymentDTO paymentDTO) {
         Map<String, String> response = new HashMap<>();
